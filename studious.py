5d8425366d33da414733dcf937f39a1b6ad08c75
Jason Perry <sh.jperry@gmail.com>
2021-06-23 11:02:16 -0500

    initial UI and main code

---
+++ studious.py
@@ -0,0 +1,20 @@
+## studious main
+
+import sys
+from PySide2.QtWidgets import QApplication, QMainWindow
+from PySide2.QtCore import QFile
+from mainwin import Ui_MainWindow
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    window = MainWindow()
+    window.show()
+
+    sys.exit(app.exec_())
